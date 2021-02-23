5fd2c47847659f0e9d5bafc6180eb5ec211f47d3
YunjinJo <black44jo@naver.com>
2021-02-24 04:21:20 +0900

    NumberWizard.cs edited Detect DownArrow, UpArrow, Enter

--- Assets/NumberWizard.cs
+++ Assets/NumberWizard.cs
@@ -7,18 +7,29 @@ public class NumberWizard : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        int max = 9999;
+        int max = 1000;
         int min = 1;
 
         Debug.Log("Welcome to Number Wizard.");
         Debug.Log("Pick a Number");
         Debug.Log("Highest numer is: " + max);
         Debug.Log("Lowest number is: " + min);
+        Debug.Log("Tell me if your number is higher or lower than 500.");
+        Debug.Log("Push Up = Higher, Push Down = Lower, Push Enter = Correct");
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+            Debug.Log("Higher");
+
+        if (Input.GetKeyDown(KeyCode.DownArrow))
+            Debug.Log("Lower");
+
+        if (Input.GetKeyDown(KeyCode.Return))
+        {
+            Debug.Log("Correct");
+        }
     }
 }
