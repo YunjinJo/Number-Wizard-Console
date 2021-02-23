ccee947281af0ef7c39b1320d0e724b0bf01c86c
YunjinJo <black44jo@naver.com>
2021-02-24 03:38:18 +0900

    NumberWizard.cs edited

--- Assets/NumberWizard.cs
+++ Assets/NumberWizard.cs
@@ -8,6 +8,9 @@ public class NumberWizard : MonoBehaviour
     void Start()
     {
         Debug.Log("Welcome to Number Wizard.");
+        Debug.Log("Pick a Number");
+        Debug.Log("Highest numer is: 25");
+        Debug.Log("Lowest number is: 1");
     }
 
     // Update is called once per frame
